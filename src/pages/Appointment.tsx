7f12adf12f19adf67d79af34afc7b1a2f7134b04
Victoria Lauri <124601042+VictoriaLauri@users.noreply.github.com>
2025-11-26 12:56:58 +0000

    app wording refactor

--- src/pages/Appointment.tsx
+++ src/pages/Appointment.tsx
@@ -30,7 +30,7 @@ export default function BookAppointment() {
           />
 
           <h1 className='text-3xl md:text-4xl font-bold text-blue-dark mb-3'>
-            Book an Appointment
+            Book a Virtual Appointment
           </h1>
 
           <p className='text-text-light max-w-2xl mx-auto'>
